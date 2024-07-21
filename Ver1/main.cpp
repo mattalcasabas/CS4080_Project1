4d998741993ee448758d59d9785ecc1c6d15605a
Matthew Alcasabas <malcasabas28@icloud.com>
2024-07-21 11:39:46 -0700

    started version 1

--- Ver1/main.cpp
+++ Ver1/main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
-    cout << "Hello world!" << endl;
-    return 0;
+// type alias for 2D vector of integers
+typedef vector<vector<int>>> Matrix;
+
+// input matrix elements from user
+void inputMatrix(Matrix &matrix, int rows, int cols) {
+    cout << "Enter matrix elements: " << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+// print matrix elements
+void printMatrix(const Matrix &matrix) {
+    for (const auto &row : matrix) {
+        for (const auto &element : row) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+}
+
+// add two matrices
+Matrix addMatrices(const Matrix &m1, const Matrix &m2, int rows, int cols) {
+    // create a result matrix
+    Matrix result(rows, vector<int>(cols));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+    return result;
+}
+
+// subtract two matrices
+Matrix subtractMatrices(const Matrix &m1, const Matrix &m2, int rows, int cols) {
+    // create a result matrix
+    Matrix result(rows, vector<int>(cols));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result[i][j] = m1[i][j] - m2[i][j];
+        }
+    }
+    return result;
+}
+
+// multiply two matrices
+Matrix multiplyMatrices(const Matrix &m1, const Matrix &m2, int rows, int cols) {
+
 }
